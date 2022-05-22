cbbc1938b55b1531a80e6f26e17d01b888ca4a68
nvudumula <vvnrpers@yahoo.com>
2022-05-22 20:34:21 +0530

    Update server.R

--- server.R
+++ server.R
@@ -1,43 +1,62 @@
-install.packages("shiny")
 library(shiny)
-library(datasets)
+install.packages("HistData")
+library(HistData)
+data(GaltonFamilies)
+library(dplyr)
+library(ggplot2)
 
-mpgData <- mtcars
-mpgData$am <- factor(mpgData$am, labels = c("Automatic", "Manual"))
+# 1st step: pass inches to cm
+gf <- GaltonFamilies
+gf <- gf %>% mutate(father=father*2.54,
+                    mother=mother*2.54,
+                    childHeight=childHeight*2.54)
+
+# linear model
+model1 <- lm(childHeight ~ father + mother + gender, data=gf)
 
 shinyServer(function(input, output) {
-  
-  formulaText <- reactive({
-    paste("mpg ~", input$variable)
+  output$pText <- renderText({
+    paste("Father's height is",
+          strong(round(input$inFh, 1)),
+          "cm, and mother's height is",
+          strong(round(input$inMh, 1)),
+          "cm, then:")
   })
-  
-  formulaTextPoint <- reactive({
-    paste("mpg ~", "as.integer(", input$variable, ")")
+  output$pred <- renderText({
+    df <- data.frame(father=input$inFh,
+                     mother=input$inMh,
+                     gender=factor(input$inGen, levels=levels(gf$gender)))
+    ch <- predict(model1, newdata=df)
+    kid <- ifelse(
+      input$inGen=="female",
+      "Daugther",
+      "Son"
+    )
+    paste0(em(strong(kid)),
+           "'s predicted height is going to be around ",
+           em(strong(round(ch))),
+           " cm"
+    )
   })
-  
-  fit <- reactive({
-    lm(as.formula(formulaTextPoint()), data=mpgData)
+  output$Plot <- renderPlot({
+    kid <- ifelse(
+      input$inGen=="female",
+      "Daugther",
+      "Son"
+    )
+    df <- data.frame(father=input$inFh,
+                     mother=input$inMh,
+                     gender=factor(input$inGen, levels=levels(gf$gender)))
+    ch <- predict(model1, newdata=df)
+    yvals <- c("Father", kid, "Mother")
+    df <- data.frame(
+      x = factor(yvals, levels = yvals, ordered = TRUE),
+      y = c(input$inFh, ch, input$inMh))
+    ggplot(df, aes(x=x, y=y, color=c("red", "green", "blue"), fill=c("red", "green", "blue"))) +
+      geom_bar(stat="identity", width=0.5) +
+      xlab("") +
+      ylab("Height (cm)") +
+      theme_minimal() +
+      theme(legend.position="none")
   })
-  
-  output$caption <- renderText({
-    formulaText()
-  })
-  
-  output$mpgBoxPlot <- renderPlot({
-    boxplot(as.formula(formulaText()), 
-            data = mpgData,
-            outline = input$outliers)
-  })
-  
-  output$fit <- renderPrint({
-    summary(fit())
-  })
-  
-  output$mpgPlot <- renderPlot({
-    with(mpgData, {
-      plot(as.formula(formulaTextPoint()))
-      abline(fit(), col=2)
-    })
-  })
-  
 })
